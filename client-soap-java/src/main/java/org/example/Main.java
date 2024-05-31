ffc53a4eadf0e32bb61db2e2c05bbfb420fb0573
HP <benmalekothman1@gmail.com>
2024-05-31 11:01:58 +0100

    TP5

--- client-soap-java/src/main/java/org/example/Main.java
+++ client-soap-java/src/main/java/org/example/Main.java
@@ -19,7 +19,6 @@ public class Main {
             System.out.println(cp.getCode());
             System.out.println(cp.getSolde());
             System.out.println(cp.getDateCreation());
-
         });
     }
 }
